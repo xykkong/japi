4ac6de117a9c216ffa0c9059d637c9a54c755d7c
Matheus Graciano <matheusgracianoandrade@gmail.com>
2014-09-25 17:14:59 -0300

    Configuracao banco de teste no ConnectionManager
    IT'S ALIVE

--- core/src/br/gov/planejamento/api/core/base/ConnectionManager.java
+++ core/src/br/gov/planejamento/api/core/base/ConnectionManager.java
@@ -10,14 +10,14 @@ public class ConnectionManager {
 	private static Connection connection = null;
 	
 	private static String username = "postgres";
-	private static String password = "123456";
+	private static String password = "DBDAdmin2005";
 	
 	public static Connection getConnection() throws SQLException {
 		if(connection == null) {
 			Properties connectionProps = new Properties();
 			connectionProps.put("user", username);
 			connectionProps.put("password", password);
-			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/teste", connectionProps);
+			connection = DriverManager.getConnection("jdbc:postgresql://146.164.34.74:5432/prototipo_api", connectionProps);
 		}
 		
 		return connection;
