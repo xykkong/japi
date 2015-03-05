49ae6ba771534648cd01b71cc6bea30a4b22e314
Villacinha <matheusandrade@ejcm.com.br>
2015-03-05 16:35:19 -0300

    DocumentationObject.java

---
+++ docs/src/br/gov/planejamento/api/docs/utils/DocumentationObject.java
@@ -0,0 +1,76 @@
+package br.gov.planejamento.api.docs.utils;
+
+import java.util.ArrayList;
+
+public class DocumentationObject {
+	
+	ArrayList<Request> requests;
+	
+	public class Request{
+		private String path;
+		private String resourceType;
+		public String getPath() {
+			return path;
+		}
+
+
+		public void setPath(String path) {
+			this.path = path;
+		}
+
+
+		public String getResourceType() {
+			return resourceType;
+		}
+
+
+		public void setResourceType(String resourceType) {
+			this.resourceType = resourceType;
+		}
+
+
+		public ArrayList<Parameter> getParameters() {
+			return parameters;
+		}
+
+
+		public void setParameters(ArrayList<Parameter> parameters) {
+			this.parameters = parameters;
+		}
+
+
+		private ArrayList<Parameter> parameters;
+		
+		
+		public class Parameter{
+			private String name;
+			private String description;
+			public String getName() {
+				return name;
+			}
+			public void setName(String name) {
+				this.name = name;
+			}
+			public String getDescription() {
+				return description;
+			}
+			public void setDescription(String description) {
+				this.description = description;
+			}
+			public String getType() {
+				return type;
+			}
+			public void setType(String type) {
+				this.type = type;
+			}
+			public String getRequired() {
+				return required;
+			}
+			public void setRequired(String required) {
+				this.required = required;
+			}
+			private String type;
+			private String required;
+		}
+	}
+}
