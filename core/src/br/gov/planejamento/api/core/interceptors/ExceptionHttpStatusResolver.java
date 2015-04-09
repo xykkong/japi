01f0d6829f0cbebe034189db14f12457cb02fce6
Pedro Barros <pedrobarros.dev@gmail.com>
2015-04-09 03:37:44 -0300

    ExceptionHttpStatusResolver
    - Tratamento das ApiExceptions lançadas

--- core/src/br/gov/planejamento/api/core/interceptors/ExceptionHttpStatusResolver.java
+++ core/src/br/gov/planejamento/api/core/interceptors/ExceptionHttpStatusResolver.java
@@ -5,75 +5,33 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 import br.gov.planejamento.api.core.exceptions.ApiException;
+import br.gov.planejamento.api.core.exceptions.CoreException;
 
 @Provider
 public class ExceptionHttpStatusResolver implements ExceptionMapper<Exception> {
 	@Override
 	public Response toResponse(Exception exception) {
-		exception.printStackTrace();
-		if(exception instanceof ApiException){
-			Exception original = ((ApiException) exception).getOriginalException();
-			if(original != null){
-				System.out.println("Exceção original:");
-				original.printStackTrace();
-			}
-		}
-		/*Response.Status httpStatus = Response.Status.INTERNAL_SERVER_ERROR;
-		
-		if(!(exception instanceof RequestException)) {
-			exception = new RequestException(exception);
+		//Se uma exception sobe sem estar encapsulada em uma ApiException, ela é encapsulada em uma CoreException
+		if(!(exception instanceof ApiException)) {
+			exception = new CoreException("Houve um erro interno desconhecido.", exception);
 		}
 		
-		ErrorResource error = new ErrorResource();
-		error.setMensagem(exception.getCause().getMessage());
-		error.setCodigo(httpStatus.getStatusCode()+"");
+		//Convertendo para ApiException
+		ApiException apiException = (ApiException) exception;
 		
-		br.gov.planejamento.api.core.base.Response response = new br.gov.planejamento.api.core.base.Response(1);
-		response.add(error);
-		response.isList(false);
-			
-		try {
-			String retorno = "";
-			switch (RequestContext.getContext().getRequestFormat()) {
-				case RequestFormats.HTML:
-					retorno = response.toHTML().toString();
-					break;
-				case RequestFormats.JSON:
-					retorno = response.toJSON();
-					break;
-				case RequestFormats.XML:
-					retorno = response.toXML();
-					break;
-				case RequestFormats.CSV:
-					retorno = response.toHTML().toString();
-					break;
-			}
-			return Response.status(httpStatus).entity(retorno).build();
-		} catch (ResourceNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseErrorException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MethodInvocationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		//Printando stackstrace
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("API EXCEPTION:");
+		apiException.printStackTrace();
+		if(apiException.getOriginalException() != null) {
+			System.out.println("-----------------------------------------------------------------");
+			System.out.println("ORIGINAL EXCEPTION:");
+			apiException.getOriginalException().printStackTrace();
+		}
+		System.out.println("-----------------------------------------------------------------");
+		
+		//Chamar aqui o getErrorResponse da ApiException em questão
+		
 		return null;
 	}	
 }
