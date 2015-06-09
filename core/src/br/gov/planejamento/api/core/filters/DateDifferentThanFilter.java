f166a4e47b4db8f6c5056117ec6e7b2c43f0ec42
Afonso Carvalho <afonsohlc@gmail.com>
2015-06-09 17:45:56 -0300

    Filtros de diferença, maioridade, minoridade (e igualdades referentes) para data

---
+++ core/src/br/gov/planejamento/api/core/filters/DateDifferentThanFilter.java
@@ -0,0 +1,70 @@
+package br.gov.planejamento.api.core.filters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.gov.planejamento.api.core.database.DatabaseAlias;
+import br.gov.planejamento.api.core.database.Filter;
+import br.gov.planejamento.api.core.parameters.DateParam;
+/**
+ * 
+ * Esse filtro deve ser utilizado unicamente para comparação de datas.
+ * 
+ * A comparação do tipo DateParam é feita exclusivamente por este filtro.
+ * 
+ * O retorno é formado por todos os resources cuja data é exatamente igual (em dia, mês e ano) ao parâmetro recebido.
+ *
+ */
+public class DateDifferentThanFilter extends Filter {
+
+	private DateDifferentThanFilter(Class<? extends Object> type, DatabaseAlias...databaseAliases) {
+		super(type, databaseAliases);
+	}
+	
+	private DateDifferentThanFilter(Class<? extends Object> type, String...parameters) {
+		super(type, parameters);
+	}
+
+	
+	@Override
+	public StringBuilder subStatement(DatabaseAlias parameterAlias) {
+		StringBuilder statement = new StringBuilder();
+		int numberOfValues = getValues(parameterAlias).size()/2;
+		statement.append(parameterAlias.getDbName());
+		statement.append(" <> ?::date");
+		for (int i=1; i<numberOfValues; i++) {
+			statement.append(" AND ");
+			statement.append(parameterAlias.getDbName());
+			statement.append(" <> ?::date");				
+		}
+		return statement;
+	}
+	@Override
+	public List<String> getValues(DatabaseAlias parameterAlias) {
+		List<String> values = new ArrayList<String>();
+		for(String value : this.values.get(parameterAlias.getUriName())){
+			values.add(value);
+		}
+		return values;
+	}
+	
+	/**
+	 * 
+	 * @param parameters Strings que contêm o nome dos parâmetros da Request cujo filtro deve ser aplicado, separados por vírgula.
+	 * 
+	 */
+	public static Filter factory(String... parameters) {
+		return new DateDifferentThanFilter(DateParam.class, parameters);
+	}
+	
+	/**
+	 * 
+	 * @param databaseAliases DatabaseAliases que contêm o nome dos parâmetros da Request cujo filtro deve ser aplicado, separados por vírgula. 
+	 * O padrão de escrita de DatabaseAlias é a seguinte: "nome_do_campo_no_banco as name_do_parametro_da_request". 
+	 * 
+	 */
+	public static Filter factory(DatabaseAlias... databaseAliases){
+		return new DateDifferentThanFilter(DateParam.class, databaseAliases);
+	}
+
+}
